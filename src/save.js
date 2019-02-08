fb3907ffcf854f696ef07f0f380ca3b3a51c6d9a
Michal Matous <michal.matous@gmail.com>
2019-02-09 00:56:00 +0100

    relatins save

--- src/save.js
+++ src/save.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { checkConstraints } from '@/constraint';
 import joinPath from 'path.join';
 
-export default async function save(keys = Object.keys(this.$toJson()), { relations = [], terminus = false } = {}) {
+export default async function save({ keys = Object.keys(this.$toJson()), relations = [], terminus = false } = {}) {
   const { post } = this.client;
 
   if (_.isUndefined(post)) {
@@ -13,12 +13,15 @@ export default async function save(keys = Object.keys(this.$toJson()), { relatio
 
   console.log(keys);
   console.log(relations);
+  console.log(this);
+  console.log(this.$toJson());
+  console.log(_.omit(_.pick(this.$toJson(), keys), '$id'));
 
 
   const path = joinPath(...relations.map(r => r.apiPath()), terminus ? this.constructor.apiPath : this.apiPath());
   console.log(path);
 
-  const data = await post(path, _.omit(_.pick(this.$toJson(), keys), '$id', '$relations', '$terminus'));
+  const data = await post(path, _.omit(_.pick(this.$toJson(), keys), '$id'));
   const stored = await this.$insert(data);
   return stored[this.constructor.entity][0];
 }
