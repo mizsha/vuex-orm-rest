755596743a631f9fc8deb73f3ba0711fe0757721
Michal Matous <michal.matous@gmail.com>
2019-04-29 14:30:59 +0200

    fetch filters

--- src/fetch.js
+++ src/fetch.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { checkConstraints } from '@/constraint';
 import joinPath from 'path.join';
 
-export default async function fetch(id) {
+export default async function fetch(id, { filter = {} } = {}) {
   const { get } = this.client;
 
   if (_.isUndefined(get)) {
@@ -13,9 +13,13 @@ export default async function fetch(id) {
     throw new Error('No id is provided');
   }
 
+  if (!_.isObject(filter)) {
+    throw new Error('Filter needs to be an object');
+  }
+
   checkConstraints(this);
 
-  const data = await get(joinPath(this.apiPath, id.toString()));
+  const data = await get(joinPath(this.apiPath, id.toString()), { params: filter });
   try {
     const insertedData = await this.insertOrUpdate(data);
     return insertedData[this.entity][0];
